74a19bb1ac1ea66dc0d0ec8960248b95dce54226
Aaron Spiteri <aaron.spiteri@staff.iinet.net.au>
2014-06-18 19:42:36 +1000

    Initial Commit

---
+++ Bounce the Shark/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Bounce the Shark
+//
+//  Created by Aaron Spiteri on 18/06/2014.
+//  Copyright AZZMOSPHERE 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Bounce the Shark/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Bounce the Shark
+//
+//  Created by Aaron Spiteri on 18/06/2014.
+//  Copyright AZZMOSPHERE 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
